ffcca49cc1d6a884b055012d37b8b2c62d93f561
Angel <angelliao1213@gmail.com>
2024-08-04 10:20:29 +0800

    Add cat-1

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,9 @@
 public class Calculator {
+
+    private String name = 'Casio';
+    private boolean status = true;
+    private dooule result = -1.0;
+
     public int add(int a, int b) {
         return a + b;
     }
